2e47e3dec419222f97f4abebc38c8859f1bea2bb
Renzo <renzo.juniorgranuzzo@hotmail.com>
2025-03-25 17:59:06 +0100

    get e post character

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,15 @@ import Router from "@koa/router";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import characterRoutes from "./routes/character.js";
+import bodyParser from "koa-bodyparser";
 
 // Init "dotenv"
 dotenv.config();
 
 const app = new Koa();
 const router = new Router();
-const prisma = new PrismaClient();
 
-router.post("/character", async (ctx) => {
-  const character = await prisma.character.create({
-    data: {
-      name: "Name 1",
-      age: 30,
-    },
-  });
-
-  ctx.status = 201;
-  ctx.body = "Character created: " + character.id;
-});
-
+app.use(bodyParser())
 app.use(characterRoutes.routes()).use(characterRoutes.allowedMethods());
 app.use(router.routes()).use(router.allowedMethods());
 
--- src/routes/character.ts
+++ src/routes/character.ts
@@ -1,15 +1,36 @@
 import Router from "@koa/router";
 import prisma from "../../prisma/client.js";
+import { Character } from "@prisma/client";
 
 const router = new Router({
   prefix: "/character",
 });
 
+// get all characters
 router.get("/", async (ctx) => {
   const characters = await prisma.character.findMany();
 
   ctx.status = 201;
   ctx.body = characters;
+});
+
+// post: create a character
+router.post("/", async (ctx) => {
+  const data = ctx.request.body as Character;
+
+  const character = await prisma.character.create({
+    data: {
+      name: data.name,
+      history: data.history,
+      age: data.age,
+      health: data.health,
+      stamina: data.stamina,
+      mana: data.mana,
+    },
+  });
+
+  ctx.status = 201;
+  ctx.body = "Character created: " + character.id;
 });
 
 export default router;
