f179c0286593e4fc123b3b6bd82d79ad3ea48e98
Alessandro Pozzato <153738740+alencrystal@users.noreply.github.com>
2025-03-27 12:29:27 +0100

    added zod for raceSchema

--- prisma/validation/validationCharacter.ts
+++ prisma/validation/validationCharacter.ts
@@ -8,3 +8,12 @@ export const characterSchema = z.object({
   stamina: z.number().int().optional(),
   mana: z.number().int().optional(),
 });
+export const raceSchema = z.object({
+  key: z.string().refine((value) => /^[a-z0-9_]+$/.test(value),{
+    message: 'Invalid machine name format',
+  }),
+  name: z.string().min(1).max(20),
+  modHealth: z.number().int(),
+  modStamina: z.number().int(),
+  modMana: z.number().int(),
+});
