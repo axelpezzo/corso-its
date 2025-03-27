5b01e5ab650a523cfbe6899753237170942b04e7
Renzo <renzo.juniorgranuzzo@hotmail.com>
2025-03-27 12:36:23 +0100

    added file validationClass.ts

--- prisma/validation/validationCharacter.ts
+++ prisma/validation/validationCharacter.ts
@@ -2,9 +2,20 @@ import { z } from "zod";
 
 export const characterSchema = z.object({
   name: z.string().min(1).max(20),
+  key: z.string().refine((value) => /^[a-z0-9_]+$/.test(value), {
+    message: "Invalid macchine name format",
+  }),
   history: z.string().optional(),
   age: z.number().int().optional(),
   health: z.number().int().optional(),
   stamina: z.number().int().optional(),
   mana: z.number().int().optional(),
+});
+
+export const classSchema = z.object({
+    name: z.string().min(1).max(20),
+    key: z.string().refine((value) => /^[a-z0-9_]+$/.test(value), {
+        message: "Invalid macchine name format",
+    }),
+    description: z.string().optional(),
 });
