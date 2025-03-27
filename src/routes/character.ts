0dc803a867c98e2fadbb5e299e3580dcbd265282
Renzo <renzo.juniorgranuzzo@hotmail.com>
2025-03-27 10:33:59 +0100

    fixed post, added patch, added middleware

--- src/routes/character.ts
+++ src/routes/character.ts
@@ -1,6 +1,7 @@
 import Router from "@koa/router";
-import prisma from "../../prisma/client.js";
+import prisma from "../../prisma/client.ts";
 import { Character } from "@prisma/client";
+import { characterExists } from "../middlewares/character.ts";
 
 const router = new Router({
   prefix: "/character",
@@ -8,29 +9,92 @@ const router = new Router({
 
 // get all characters
 router.get("/", async (ctx) => {
-  const characters = await prisma.character.findMany();
-
-  ctx.status = 201;
-  ctx.body = characters;
+  try {
+    const characters = await prisma.character.findMany();
+    ctx.status = 201;
+    ctx.body = characters;
+  } catch (error){
+    ctx.status = 500;
+    ctx.body = "Error: " + error;
+  }
+  
 });
 
 // post: create a character
 router.post("/", async (ctx) => {
   const data = ctx.request.body as Character;
+  try {
+    const character = await prisma.character.create({
+      data: {
+        name: data.name,
+        history: data.history,
+        age: data.age,
+        health: data.health,
+        stamina: data.stamina,
+        mana: data.mana,
+      },
+    });
 
-  const character = await prisma.character.create({
-    data: {
-      name: data.name,
-      history: data.history,
-      age: data.age,
-      health: data.health,
-      stamina: data.stamina,
-      mana: data.mana,
-    },
+    ctx.status = 201;
+    ctx.body = "Character created: " + character.id;
+  } catch (error){
+    ctx.status = 500;
+    ctx.body = "Error: " + error;
+  }
+});
+
+// get one character, given the id
+router.get("/:id", async (ctx) => {
+  const id = ctx.params.id;
+
+  try{
+    const character = await prisma.character.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!character){
+      ctx.status = 404;
+      ctx.body = "Character not found";
+      return;
+    }
+    else {
+      ctx.status = 201;
+      ctx.body = character;
+    }
+  }catch (error){
+    ctx.status = 500;
+    ctx.body = "Error: " + error;
+  }
+  
+  // patch
+  router.patch("/:id", (ctx, next) => characterExists(ctx, next), async (ctx) => {
+    const id = ctx.params.id;
+    const data = ctx.request.body as Character;
+
+    try{
+      const character = await prisma.character.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name: data.name,
+          history: data.history,
+          age: data.age,
+          health: data.health,
+          stamina: data.stamina,
+          mana: data.mana,
+        },
+      });
+
+      ctx.status = 201;
+      ctx.body = "Character updated: " + character.id;
+    } catch (error){
+      ctx.status = 500;
+      ctx.body = "Error: " + error;
+    }
   });
-
-  ctx.status = 201;
-  ctx.body = "Character created: " + character.id;
 });
 
 export default router;
