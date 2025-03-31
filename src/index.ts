1b215eac18a7201e9c790ba972dd9e2e3e0524e9
Alex Pezzini <alex.pezzini@intesys.it>
2025-03-31 12:13:27 +0200

    Add to index

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import Koa from "koa";
 import Router from "@koa/router";
 import dotenv from "dotenv";
 import characterRoutes from "./routes/character";
-import attribureRoutes from "./routes/attribute";
+import attributeRoutes from "./routes/attribute";
 import raceAttrModRoutes from "./routes/raceAttrMod";
 import classRoutes from "./routes/class";
+import skillRoutes from "./routes/skill";
+import raceRoute from "./routes/race";
 import bodyParser from "koa-bodyparser";
 import classSkillModRoutes from "./routes/classSkillMod";
 
@@ -21,10 +23,12 @@ router.get("/", (ctx) => {
 });
 
 app.use(characterRoutes.routes()).use(characterRoutes.allowedMethods());
-app.use(attribureRoutes.routes()).use(attribureRoutes.allowedMethods());
+app.use(attributeRoutes.routes()).use(attributeRoutes.allowedMethods());
 app.use(classSkillModRoutes.routes()).use(classSkillModRoutes.allowedMethods());
 app.use(raceAttrModRoutes.routes()).use(raceAttrModRoutes.allowedMethods());
 app.use(classRoutes.routes()).use(classRoutes.allowedMethods());
+app.use(skillRoutes.routes()).use(skillRoutes.allowedMethods());
+app.use(raceRoute.routes()).use(raceRoute.allowedMethods());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(process.env.APP_PORT || 3000, () => {
