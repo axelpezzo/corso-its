e6e2df2db9d8f00b13785b223606af7bbd2ee342
Matteo Scandolara <matteo.scando@gmail.com>
2025-03-31 11:04:01 +0200

    added classSkillMod routes and updated index
    Add new routes for handling class-skill modifications, including GET and POST endpoints. Update the server initialization to include the new routes and add a root endpoint for basic server response. Enhance server startup logging to display the running port.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Router from "@koa/router";
 import dotenv from "dotenv";
 import characterRoutes from "./routes/character.js";
 import bodyParser from "koa-bodyparser";
+import classSkillModRoutes from "./routes/classSkillMod.ts"
 
 // Init "dotenv"
 dotenv.config();
@@ -12,7 +13,20 @@ const router = new Router();
 
 app.use(bodyParser());
 
+router.get("/", (ctx) => {
+    ctx.response.body = "GDR Node";
+});
+
 app.use(characterRoutes.routes()).use(characterRoutes.allowedMethods());
+app.use(classSkillModRoutes.routes()).use(classSkillModRoutes.allowedMethods());
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(process.env.APP_PORT || 3000);
+
+
+
+
+
+
+app.listen(process.env.APP_PORT || 3000, () => {
+    console.log(`HTTP Server running on http://localhost:${process.env.APP_PORT}`);
+});
