18b6919114c43b055278a1037cebf99b51b54948
GiacoFra24 <giacomo.bertoldi24@gmail.com>
2025-04-01 10:16:20 +0200

    update crud

--- src/routes/raceAttrMod.ts
+++ src/routes/raceAttrMod.ts
@@ -4,6 +4,7 @@ import { RaceAttrMod } from "@prisma/client";
 import { RaceAttrModSchema } from "../../prisma/validation/validationRaceAttrMod";
 import { validationError } from "../utilities/errorsHandler";
 import { ZodError } from "zod";
+import { error } from "console";
 
 const router = new Router();
 
@@ -18,6 +19,44 @@ router.get("/race/attr/mod", async (ctx) => {
     ctx.body = "Error: " + error;
   }
 });
+
+router.get("/race/:idRace/attr/:idAttribute", async (ctx) => {
+    
+  try {   
+    ctx.request.body = RaceAttrModSchema.parse(ctx.request.body);
+    const data = ctx.request.body as RaceAttrMod;
+
+    const idRace = ctx.params.idRace;
+    const idAttribute = ctx.params.idAttribute;
+
+    if (!idRace || !idAttribute) {
+      ctx.status = 400;
+      ctx.body = "Error: idRace and idAttribute are required";
+      return;
+    }
+
+    const raceAttrMod = await prisma.raceAttrMod.findUnique({
+      where : {
+        idRace_idAttribute: {
+          idRace: idRace,
+          idAttribute: idAttribute,
+        }
+      },
+    });
+
+    ctx.status = 200;
+    ctx.body = raceAttrMod;
+  
+  } catch (error) {
+    ctx.status = 500;
+    if (error instanceof ZodError) {
+      ctx.body = validationError(error);
+    } else {
+      ctx.body = "Generic Error: " + error;
+    }
+  }
+
+})
 
 // POST /: create a raceAttrMod
 router.post("/race/:idRace/attr/:idAttribute", async (ctx) => {
@@ -50,6 +89,96 @@ router.post("/race/:idRace/attr/:idAttribute", async (ctx) => {
         raceAttrMod.idRace +
         "idAttribute: " +
         raceAttrMod.idAttribute;
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = "Error: " + error;
+    }
+  } catch (error) {
+    ctx.status = 500;
+    if (error instanceof ZodError) {
+      ctx.body = validationError(error);
+    } else {
+      ctx.body = "Generic Error: " + error;
+    }
+  }
+});
+
+// PATCH /: update a raceAttrMod
+router.patch("/race/:idRace/attr/:idAttribute", async (ctx) => {
+  try {
+    ctx.request.body = RaceAttrModSchema.parse(ctx.request.body);
+    const data = ctx.request.body as RaceAttrMod;
+
+    const idRace = ctx.params.idRace;
+    const idAttribute = ctx.params.idAttribute;
+
+    if (!idRace || !idAttribute) {
+      ctx.status = 400;
+      ctx.body = "Error: idRace and idAttribute are required";
+      return;
+    }
+
+    try {
+      const raceAttrMod = await prisma.raceAttrMod.update({
+        where: {
+          idRace_idAttribute: {
+            idRace: idRace,
+            idAttribute: idAttribute,
+          }
+        },
+        data: {
+          value: data.value,
+        },
+      });
+
+      ctx.status = 200;
+      ctx.body =
+        "Race Attribute Relation updated: " +
+        "idRace: " +
+        raceAttrMod.idRace +
+        "idAttribute: " +
+        raceAttrMod.idAttribute;
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = "Error: " + error;
+    }
+  } catch (error) {
+    ctx.status = 500;
+    if (error instanceof ZodError) {
+      ctx.body = validationError(error);
+    } else {
+      ctx.body = "Generic Error: " + error;
+    }
+  }
+})
+
+// DELETE /: delete a raceAttrMod
+router.delete("/race/:idRace/attr/:idAttribute", async (ctx) => {
+  try {
+    ctx.request.body = RaceAttrModSchema.parse(ctx.request.body);
+    const data = ctx.request.body as RaceAttrMod;
+
+    const idRace = ctx.params.idRace;
+    const idAttribute = ctx.params.idAttribute;
+
+    if (!idRace || !idAttribute) {
+      ctx.status = 400;
+      ctx.body = "Error: idRace and idAttribute are required";
+      return;
+    }
+
+    try {
+      const raceAttrMod = await prisma.raceAttrMod.delete({
+        where: {
+          idRace_idAttribute: {
+            idRace: idRace,
+            idAttribute: idAttribute,
+          }
+        }
+      });
+
+      ctx.status = 200;
+      ctx.body = "Race Attribute Relation deleted correctly"
     } catch (error) {
       ctx.status = 500;
       ctx.body = "Error: " + error;
