c4fbf948c24a67c0c977bc9cc0af5bfef29e68a9
GiacoFra24 <giacomo.bertoldi24@gmail.com>
2025-04-03 12:12:29 +0200

    update docu api raceAttrMod

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ import bodyParser from "koa-bodyparser";
 import userRouter from "./routes/user";
 import meRouter from "./routes/me";
 import classSkillModRoutes from "./routes/classSkillMod";
+import swaggerJSDoc from "swagger-jsdoc";
+import { koaSwagger } from 'koa2-swagger-ui';
+
 
 // Init "dotenv"
 dotenv.config();
@@ -23,6 +26,35 @@ app.use(bodyParser());
 router.get("/", (ctx) => {
   ctx.response.body = "GDR Node";
 });
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Documentazione DGR Node',
+      version: '1.0.0'
+    },
+    servers: [
+      {
+          url: `http://localhost:${process.env.APP_PORT || 3000}`,
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts']
+}
+
+const swaggerSpec= swaggerJSDoc(swaggerOptions);
+
+router.get("/swagger.json", (ctx) => {
+  ctx.response.body = swaggerSpec
+});
+
+app.use(koaSwagger({
+  routePrefix: "/docs",
+  swaggerOptions: {
+    url: '/swagger.json'
+  }
+}));
 
 app.use(characterRoutes.routes()).use(characterRoutes.allowedMethods());
 app.use(attributeRoutes.routes()).use(attributeRoutes.allowedMethods());
@@ -37,6 +69,8 @@ app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(process.env.APP_PORT || 3000, () => {
   console.log(
-    `HTTP Server running on http://localhost:${process.env.APP_PORT}`
+    `Server running --> http://localhost:${process.env.APP_PORT || 3000}\n`
+    +
+    `Docs section --> http://localhost:${process.env.APP_PORT || 3000}/docs`
   );
 });
