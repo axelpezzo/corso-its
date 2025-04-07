f28d00e24553eb120bdf758c38c2071a513dd37e
macs-massimopiazza <me@massimopiazza.com>
2025-04-07 10:25:12 +0200

    swagger init in separate folder

--- src/doc/swagger.ts
+++ src/doc/swagger.ts
@@ -1,26 +1,38 @@
+import Router from "@koa/router";
 import swaggerJSDoc from "swagger-jsdoc";
-import koa from "koa";
+import { koaSwagger } from "koa2-swagger-ui";
 
+import fs from "fs";
+import path from "path";
+import yaml from "yaml";
 
-const app = new koa();
-
-// Configurazione di Swagger
-const swaggerOptions = {
+const swaggerOptions ={
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "Swagger API",
-      version: "1.0.0",
-      description: "Documentazione DGR Node",
+      title: 'Documentazione GDR Node',
+      version: '1.0.0',
+      description: 'API Documentation of our gdr',
     },
-    servers: [
-      {
-        url: "http://localhost:3000",
-        description: "Server locale",
-      },
-    ], 
   },
-  apis: ["./src/routes/*.ts"]
+  apis: ['./src/routes/*.ts'],
 };
 
-export const swaggerSpec = swaggerJSDoc(swaggerOptions);
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+console.log(swaggerSpec);
+
+const yamlData = yaml.stringify(swaggerSpec);
+console.log(yamlData)
+fs.writeFileSync(path.join('openapi.yaml'), yamlData);
+
+export const swaggerRoute = new Router().get('/swagger.json', (ctx) => {
+    ctx.set('Content-Type', 'application/json');
+    ctx.body = swaggerSpec;
+});
+
+export const swaggerUI = koaSwagger({
+    routePrefix: "/docs",
+    swaggerOptions: {
+        url: "/swagger.json"
+    }
+});
