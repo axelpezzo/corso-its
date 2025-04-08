0d17678b5f9cae440d2da30791ed6900da2ee541
GiacoFra24 <giacomo.bertoldi24@gmail.com>
2025-04-08 12:43:15 +0200

    update + frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@mantine/core"
+
+const Page = () => {
+  return <div>Game ITS</div>;
+  <Button color="lime.4" autoContrast>Lime.4 button</Button>
+};
+
+export default Page;
