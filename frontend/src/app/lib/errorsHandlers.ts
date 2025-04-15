cee964ba80ba4095b5a300b1652a5b5194b2982d
Alex Pezzini <alex.pezzini@intesys.it>
2025-04-15 15:34:12 +0200

    FE: Login

--- frontend/src/app/lib/errorsHandlers.ts
+++
@@ -1,9 +0,0 @@
-export const errorsHandlers = (error: number) => {
-  switch (error) {
-    case 401:
-      return "Invalid email or password";
-    default:
-    case 500:
-      return "Internal Server Error";
-  }
-};
--- frontend/src/components/ui/navbar/index.tsx
+++ frontend/src/components/ui/navbar/index.tsx
@@ -1,37 +1,61 @@
-/*import { Code, Group } from "@mantine/core";
+import { useState } from "react";
+import {
+  IconUserCircle,
+  IconLogout,
+  IconPlayerPlayFilled,
+  IconSettings,
+} from "@tabler/icons-react";
+import { Group, Title } from "@mantine/core";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import classes from "./styles.module.css";
+import Link from "next/link";
+
+const data = [
+  { link: "/play", label: "Play", icon: IconPlayerPlayFilled },
+  { link: "/character", label: "Character", icon: IconUserCircle },
+  { link: "/options", label: "Options", icon: IconSettings },
+];
 
 const Navbar = () => {
+  const [active, setActive] = useState("Billing");
+
+  const links = data.map((item) => (
+    <Link
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={() => {
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} size={32} />
+      <Title order={4}>{item.label}</Title>
+    </Link>
+  ));
+
   return (
     <nav className={classes.navbar}>
       <div className={classes.navbarMain}>
         <Group className={classes.header} justify="space-between">
-          <Code fw={700} className={classes.version}>
-            v0.0.1
-          </Code>
+          <Image src={logo} width={1280} height={720} alt={"App Logo"} />
         </Group>
         {links}
       </div>
 
       <div className={classes.footer}>
-        <a
+        <Link
           href="#"
           className={classes.link}
           onClick={(event) => event.preventDefault()}
         >
-          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
-          <span>Change account</span>
-        </a>
-
-        <a
-          href="#"
-          className={classes.link}
-          onClick={(event) => event.preventDefault()}
-        >
-          <IconLogout className={classes.linkIcon} stroke={1.5} />
-          <span>Logout</span>
-        </a>
+          <IconLogout className={classes.linkIcon} stroke={1.5} size={32} />
+          <Title order={4}>Logout</Title>
+        </Link>
       </div>
     </nav>
   );
 };
-*/
+
+export default Navbar;
